303118a8874a117ae9bbc3631a318bd09264aaa6
Obed Vusumuzi Masombuka <vusimasombuka@gmail.com>
2026-02-22 02:56:18 +0200

    Switch to psycopg3

--- config.py
+++ config.py
@@ -8,15 +8,14 @@ class Config:
     db_url = os.getenv("DATABASE_URL")
 
     if db_url:
-        # Fix Render postgres:// issue
         if db_url.startswith("postgres://"):
-            db_url = db_url.replace("postgres://", "postgresql://", 1)
+            db_url = db_url.replace("postgres://", "postgresql+psycopg://", 1)
+
+        if db_url.startswith("postgresql://"):
+            db_url = db_url.replace("postgresql://", "postgresql+psycopg://", 1)
 
         SQLALCHEMY_DATABASE_URI = db_url
     else:
-        # Local development
         SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'instance', 'cms.db')}"
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    EMERGENCY_ACCESS = True
