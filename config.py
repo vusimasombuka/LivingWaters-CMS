34f9d67b45ae97654325b07ce4596ab4392dd62b
Obed Vusumuzi Masombuka <vusimasombuka@gmail.com>
2026-02-27 18:44:48 +0200

    Added Server URL custom domain

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
+SERVER_NAME = os.getenv("SERVER_NAME", None)
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
 
